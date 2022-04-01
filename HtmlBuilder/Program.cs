dbe64a3ecb36eb8cdc15e6640b3ec3a65fb3bcbb
Cezar Iancu <cezar.iancu@888holdings.com>
2022-04-01 17:29:07 +0300

    first task

---
+++ HtmlBuilder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HtmlBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> ValueList = new List<string>();
+            string userInput;
+            do
+            {
+                {
+                    Console.WriteLine("Enter input:");
+                    userInput = Console.ReadLine().ToLower();
+                    ValueList.Add(userInput);
+                    foreach (string i in ValueList)
+                    {
+                        Console.WriteLine("ai in list {0}", i);
+                    }
+
+                }
+            }
+            while (userInput != "exit");
+
+        }
+    }
+}
